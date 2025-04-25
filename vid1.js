fe7e559608ccb2c60a946184390e0e670d8efbd3
Sir-Rubalot <robinekbladh@hotmail.com>
2025-04-25 19:27:09 +0200

    Add files via upload

---
+++ vid1.js
@@ -0,0 +1,12 @@
+console.log("Multiplikationsfunktion");
+
+let number1 = prompt ("Skriv ett tal");
+
+let number2 = prompt("Skriv ytterligare ett tal");
+
+function multiply(number1: number, number2: number) : number {
+
+    var sum = number1 * number2;
+
+    console.log(sum);
+}
